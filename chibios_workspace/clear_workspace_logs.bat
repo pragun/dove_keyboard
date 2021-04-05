47d120862965a38b2a27225fbb3db05cda143f7c
pragun <pragun.goyal@gmail.com>
2021-04-05 16:10:08 -0400

    Moves tmk_test to the root of the repo.

--- chibios_workspace/clear_workspace_logs.bat
+++
@@ -1,4 +0,0 @@
-del /S .\.metadata\*.log
-del /S .\.metadata\*.pdom
-del /S .\.metadata\*.sc
-pause
--- stm32f4_usb_device/tmk_interface/wait_api.h
+++
@@ -1,4 +0,0 @@
-#   define wait_ms(ms)  HAL_Delay(ms)
-#   define wait_us(us)  _delay_us(us)
-
-void _delay_us(uint16_t us);
