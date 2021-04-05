2d12f9dff47cb5b55a5b800c98dc93ec3b4ad8c1
pragun <pragun.goyal@gmail.com>
2021-04-04 21:55:25 -0400

    Final keymap adjustment of the day

--- chibios_workspace/tmk_test/keymap_plain.c
+++ chibios_workspace/tmk_test/keymap_plain.c
@@ -23,8 +23,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap.h"
 #include "keymap_common.h"
 
+
 const uint8_t keymaps[][5][25] = {
- /* 0: Qwerty
+ /* 0: Qwerty: The graphic is deprecated now
+  * as on 4/4/2021
       * ,-----------------------------------------------------------------------------------------------------------------------.
       * |Esc |F12|F11|F10|  ~`| 1|  2|  3|  4|  5|  6|  PrnScn|  Lock | Pause| 7 | 8 | 9 | 0 | - | = |BkSpc| NumLck| / | * | - |
       * |----------------------------------------------------------------------------------------------------------------------|
@@ -42,22 +44,22 @@ const uint8_t keymaps[][5][25] = {
      KEYMAP(NLCK, F12, F11, F10, ESC, GRV, 1,   2,   3,   4,   5, PSCR, SLCK, BRK,  6, 7, 8 , 9, 0, MINS, EQL, NLCK, PSLS, PAST, PMNS,
             PPLS, F9, F8, F7, TAB, Q, W, E, R, T, INS, HOME, PGUP, Y, U, I, O, P, LBRC, RBRC, P7, P8, P9, PPLS,
             F6, F5, F4, FN0, FN1, A, S, D, F, G, DEL, END, PGDN, H, J, K, L, SCLN, QUOT, BSLS, P4, P5, P6,
-            PENT, F3, F2, F1, LSFT, Z, X, C, V, B, UP, N, M, COMM, DOT, SLSH, RSFT, P1, P2, P3, PENT,
-            PDOT, PDOT, LCTL, LGUI, NO, LALT, SPC, LEFT, DOWN, RIGHT, ENT, RALT, RGUI, RCTRL, BSPC, P0, PDOT,
+            PENT, F3, F2, F1, LSFT, Z, X, C, V, B, UP, N, M, COMM, DOT, SLSH, ENT, P1, P2, P3, PENT,
+            PDOT, PDOT, LCTL, LGUI, NO, LALT, SPC, LEFT, DOWN, RIGHT, RSFT, BSPC, RGUI, RCTRL, DEL, P0, PDOT,
             NO, NO),
 
      KEYMAP(TRNS, TRNS, TRNS, TRNS, ESC, TRNS, F1,  F2, F3, F4, F5, TRNS, TRNS, TRNS, F6, F7, F8 , F9, F10, F11, F12, TRNS, TRNS, TRNS, BSPC,
             TRNS, 9, 8, 7, TRNS, TRNS, UP, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, UP, END, PGUP, TRNS, TRNS, HOME, UP, PGUP, DEL,
             6, 5, 4, FN0, FN1, LEFT, DOWN, RIGHT, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, LEFT, DOWN, RIGHT, PGDN, TRNS,  TRNS, LEFT, ENT, RIGHT,
            TRNS, 3, 2, 1, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, END, DOWN, PGDN, ENT,
-           TRNS, DEL, TRNS, TRNS, TRNS, TRNS, BSPC, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, DEL, ESC, TRNS,
+           TRNS, DEL, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, ESC, TRNS,
            NO, NO),
 
-     KEYMAP(TRNS, TRNS, TRNS, TRNS, ESC, TRNS, F1,  F2, F3, F4, F5, TRNS, TRNS, TRNS, F6, F7, F8 , F9, F10, F11, F12, TRNS, TRNS, TRNS, TRNS,
-            TRNS, 9, 8, 7, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, HOME, UP, END, PGUP, TRNS, TRNS, P7, P8, P9, PPLS,
-            6, 5, 4, FN0, FN1, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, LEFT, DOWN, RIGHT, PGDN, TRNS,  TRNS, TRNS, TRNS, TRNS,
-            TRNS, 3, 2, 1, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
-            TRNS, DEL, TRNS, TRNS, TRNS, TRNS, DEL, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, DEL, TRNS, TRNS, TRNS,
+     KEYMAP(TRNS, TRNS, TRNS, TRNS, ESC, TRNS, F1,  F2, F3, F4, F5, TRNS, TRNS, TRNS, F6, 7, 8 , 9, 0, F11, F12, TRNS, TRNS, TRNS, TRNS,
+            TRNS, 9, 8, 7, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, 4, 5, 6, PPLS, TRNS, TRNS, P7, P8, P9, PPLS,
+            6, 5, 4, FN0, FN1, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, 1, 2, 3, PMNS, TRNS,  TRNS, TRNS, TRNS, TRNS,
+            TRNS, 3, 2, 1, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, 0, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
+            TRNS, DEL, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
             NO, NO)
 };
 
